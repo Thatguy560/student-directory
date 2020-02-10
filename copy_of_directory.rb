c379ec4a42f2dc92f57dbeb401f1c65b9babbfac
Peter <Peterstevens510@gmail.com>
2020-02-10 19:46:48 +0000

    changes made

--- copy_of_directory.rb
+++ copy_of_directory.rb
@@ -18,8 +18,10 @@ puts "-------------"
 end
 
 def print(students)
-students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do |student, index|
+  if student[:name].start_with?("P") && (student[:name]).length < 12
+  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+end
 end
 end
 
